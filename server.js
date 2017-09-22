2cde1870addf7013a3132ed08518e3e6ba64d865
Annabel Lin <annabel.lin@insydesw.com.tw>
2017-09-22 17:27:32 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,305 @@
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+var connectList = {};
+var clientList = [];
+var targetList = [];
+var todclientList = [];
+var todtargetList = [];
+var pktadapterList = [];
+var pktmobileList = [];
+var uuidList = [];
+var uuidMax = 100;
+
+function guid() {
+    function n6() {
+        return Math.floor((Math.random()) * 1000000).toString(10);
+    }
+    function padLeft(str, lenght) {
+        if (str.length >= lenght)
+            return str;
+        else
+            return padLeft("0" + str, lenght);
+    }
+    return padLeft(n6(), 6);
+}
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+
+  var uuid1 = null
+  while (!uuid1) {
+      var tmp = guid();
+      if (uuidList.indexOf(tmp) < 0) {
+          uuid1 = tmp;
+      }
+  }
+  uuidList.push(uuid1);
+
+  ws.on('message', function incoming(message) {
+      this.uuid = uuid1;
+      //connection = this;
+      //connection.uuid = uuid1;
+      connectList[uuid1] = {
+          "connection": this,
+          "deviceuuid": "",
+          "nickname": "",
+          "pinCode": "",
+          "version": "",
+          "meetingtitle": "",
+          "meetingperiod": "",
+          "attendcount": "",
+      };
+      try {
+          var msg = JSON.parse(message);
+          if (msg.command) {
+              switch (msg.command) {
+                  case 'registerdevice':
+                      //console.log('Receive registerdevice');
+                      if (msg.message.devicetype === 'target' || msg.message.devicetype === 'tod-target') {
+                          if (!msg.message.uuid) {
+                              console.log("1wss user " + msg.message.devicetype + ":", this.uuid, "registerdevice type 1: no uuid info, use default uuid:", msg.message.uuid);
+                          } else if (msg.message.uuid === this.uuid) {
+                              console.log("2wss user " + msg.message.devicetype + ":", this.uuid, "registerdevice type 2: uuid info is equal to default uuid:", msg.message.uuid);
+                          } else if (uuidList.indexOf(msg.message.uuid) >= 0 && connectList[msg.message.uuid].deviceuuid.length == 0) {
+                              console.log("3wss user " + msg.message.devicetype + ":", this.uuid, "registerdevice type 3: uuid info is duplicateed, skip create new uuie:", msg.message.uuid);
+                          } else if (uuidList.indexOf(msg.message.uuid) >= 0 && connectList[msg.message.uuid].deviceuuid.length >= 0) {
+                              console.log("4wss user " + msg.message.devicetype + ":", this.uuid, "registerdevice type 4: device is reconnect and deviceuuid is the same, use old setting:", msg.message.uuid, msg.message.deviceuuid);
+                              console.log("5wss uuid:", this.uuid, "change to", msg.message.uuid);
+                              connectList[msg.message.uuid].connection.overwrite = true;
+                              connectList[msg.message.uuid].connection = connectList[this.uuid].connection;
+                              var index = uuidList.indexOf(this.uuid);
+                              uuidList.splice(index, 1);
+                              delete connectList[this.uuid];
+
+                              //connectList[msg.message.uuid].connection.uuid = msg.message.uuid;
+                              this.uuid = msg.message.uuid; //-> this.uuid is equal to connectList[msg.message.uuid].connection.uuid
+
+                          } else {
+                              console.log('6wss ' + "user " + msg.message.devicetype + ":", uuid, "registerdevice type 5: uuid info is not duplicateed, use self's own uuid:", msg.message.uuid);
+                              console.log('7wss ' + "uuid:", this.uuid, "change to", msg.message.uuid);
+                              connectList[msg.message.uuid] = connectList[this.uuid];
+                              var index = uuidList.indexOf(uuid);
+                              uuidList.splice(index, 1);
+                              delete connectList[uuid1];
+                              this.uuid = msg.message.uuid;
+                              uuidList.push(this.uuid);
+                          }
+
+                          connectList[this.uuid].deviceuuid = msg.message.deviceuuid;
+                          connectList[this.uuid].roomid = this.uuid;
+                          if (msg.message.devicetype === 'target' && targetList.indexOf(this.uuid) < 0) targetList.push(this.uuid);
+                          if (msg.message.devicetype === 'tod-target') {
+                              if (todtargetList.indexOf(this.uuid) < 0) todtargetList.push(this.uuid);
+                              PostCode(GetPostdataForToD(this.uuid, 1), ToDWebService.host, ToDWebService.setTargetStatus);
+                          }
+
+                          //PostCode(GetPostdata(this.uuid, 1), CKTWebService.host, CKTWebService.setTargetStatusWithMeetingInfo);
+                          connectList[this.uuid].connection.send(JSON.stringify({
+                              sender: 'Server',
+                              date: getTime(),
+                              command: 'assignuuid',
+                              message: {
+                                  uuid: this.uuid,
+                              }
+                          }));
+                      } else if (msg.message.devicetype === 'client') {
+                          if (clientList.indexOf(this.uuid) < 0) clientList.push(this.uuid);
+                      } else if (msg.message.devicetype === 'tod-client') {
+                          //console.log('wss tod-client:', todclientList.indexOf(this.uuid), todclientList, this.uuid);
+                          if (todclientList.indexOf(this.uuid) < 0) todclientList.push(this.uuid);
+                      } else if (msg.message.devicetype === 'pkt-mobile' || msg.message.devicetype === 'pkt-adapter') {
+                          if (pktmobileList.indexOf(this.uuid) < 0 && msg.message.devicetype === 'pkt-mobile')
+                              pktmobileList.push(this.uuid);
+                          if (pktadapterList.indexOf(this.uuid) < 0 && msg.message.devicetype === 'pkt-adapter')
+                              pktadapterList.push(this.uuid);
+                          connectList[this.uuid].connection.send(JSON.stringify({
+                              sender: 'Server',
+                              date: getTime(),
+                              command: 'registerdeviceresult',
+                              message: {
+                                  id: this.uuid,
+                              }
+                          }));
+                      } else {
+                          console.log('8wss ' + 'registerdevice error: ' + msg.message);
+                          break;
+                      }
+
+                      console.log('9wss ' + 'user', msg.message.devicetype, ': ' + this.uuid + ', ' + getTime() + ' is registered.');
+                      console.log("user's deviceuuid:", connectList[this.uuid].deviceuuid);
+                      console.log("  ## total uuid number:", uuidList.length);
+                      console.log("    -> current uuid list: ", uuidList);
+                      console.log("  ## total target number:", targetList.length);
+                      console.log("    -> current target list: ", targetList);
+                      console.log("  ## total client number:", clientList.length);
+                      console.log("    -> current client list: ", clientList);
+                      console.log("  ## total ToD-target number:", todtargetList.length);
+                      console.log("    -> current ToD-target list: ", todtargetList);
+                      console.log("  ## total ToD-client number:", todclientList.length);
+                      console.log("    -> current ToD-client list: ", todclientList);
+                      console.log("  ## total PassKanTan mobile number:", pktmobileList.length);
+                      console.log("    -> current PassKanTan mobile list: ", pktmobileList);
+                      console.log("  ## total PassKanTan adapter number:", pktadapterList.length);
+                      console.log("    -> current PassKanTan adapter list: ", pktadapterList);
+                      console.log('\n');
+                      break;
+
+                  case 'updateinfo':
+                      // only for Target side, provide a way for Target to update Target's nickname/pinCode/version
+                      connectList[this.uuid].nickname = (msg.message.nickname) ? msg.message.nickname : "";
+                      connectList[this.uuid].pinCode = (msg.message.pinCode) ? msg.message.pinCode : "";
+                      connectList[this.uuid].version = (msg.message.version) ? msg.message.version : "";
+                      connectList[this.uuid].meetingperiod = (msg.message.meetingperiod) ? msg.message.meetingperiod : "";
+                      connectList[this.uuid].attendcount = (msg.message.attendcount) ? msg.message.attendcount : "";
+                      connectList[this.uuid].meetingtitle = (msg.message.meetingtitle) ? msg.message.meetingtitle : "";
+
+                      PostCode(GetPostdata(this.uuid, 1), CKTWebService.host, CKTWebService.setTargetStatusWithMeetingInfo);
+
+                      console.log('wss ' + 'user:', this.uuid, 'update info:', connectList[this.uuid].nickname, connectList[this.uuid].pinCode);
+                      console.log('\n');
+                      // don't send target info throught websocket, Client will request target info by WebService
+                      break;
+                      var roominfo = [];
+                      roominfo.push({
+                          "nickname": connectList[this.uuid].nickname,
+                          "pinCode": connectList[this.uuid].pinCode,
+                          "version": connectList[this.uuid].version,
+                          "roomid": this.uuid,
+                      });
+                      for (var i = 0; i < clientList.length; i++) {
+                          connectList[clientList[i]].connection.send(JSON.stringify({
+                              sender: 'Server',
+                              date: getTime(),
+                              command: 'targetroominfo',
+                              message: {
+                                  roominfo: roominfo,
+                                  roomremove: [],
+                              }
+                          }));
+                      }
+                      break;
+
+                  case 'requestroominfo':
+                      // only for Client side, query Target info.
+                      var roominfo = [];
+                      for (var i = 0; i < targetList.length; i++) {
+                          roominfo.push({
+                              "nickname": connectList[targetList[i]].nickname,
+                              "pinCode": connectList[targetList[i]].pinCode,
+                              "version": connectList[targetList[i]].version,
+                              "roomid": connectList[targetList[i]].roomid,
+                          });
+                      }
+                      connectList[this.uuid].connection.send(JSON.stringify({
+                          sender: 'Server',
+                          date: getTime(),
+                          command: 'targetroominfo',
+                          message: {
+                              roominfo: roominfo,
+                              roomremove: [],
+                          }
+                      }));
+                      break;
+
+                  case 'requestuuid':
+                      // Client request uuid from Target through this command
+                      if (targetList.indexOf(msg.message.toId) >= 0 || todtargetList.indexOf(msg.message.toId) >= 0) {
+                          msg.message.fromServerId = this.uuid;
+                          connectList[msg.message.toId].connection.send(JSON.stringify({
+                              sender: 'Server',
+                              date: getTime(),
+                              command: 'requestuuid',
+                              message: msg.message,
+                          }));
+
+                          //ws.send(JSON.stringify({
+                          //    sender: 'Server',
+                          //    date: getTime(),
+                          //    command: 'requestuuid',
+                          //    message: msg.message,
+                          //}));
+                      }
+                      break;
+
+                  case 'assignuuid':
+                      // Target assign uuid to Client through this command
+                      if (msg.message.toServerId && uuidList.indexOf(msg.message.toServerId) >= 0) {
+                          connectList[msg.message.toServerId].connection.send(JSON.stringify({
+                              sender: 'Server',
+                              date: getTime(),
+                              command: 'assignuuid',
+                              message: {
+                                  "uuid": msg.message.uuid,
+                                  "roomId": msg.message.roomId,
+                                  "fromDevicetype": msg.message.fromDevicetype,
+                              }
+                          }));
+                      }
+                      break;
+
+                  case 'signal':
+                      //# Any device CANNOT send signal without uuid
+                      //# It means devices must registerdevice before send signal message
+                      if (!this.uuid) break;
+                      if (msg.message && uuidList.indexOf(msg.message.toServerId) >= 0) {
+                          msg.message.info = {
+                              serverId: this.uuid,
+                              fromDevicetype: msg.message.deviceType,
+                          };
+                          connectList[msg.message.toServerId].connection.send(JSON.stringify({
+                              sender: 'Server',
+                              date: getTime(),
+                              command: msg.command,
+                              message: msg.message
+                          }));
+                      }
+                      break;
+
+                  case 'checktodtarget':
+                      connectList[this.uuid].connection.send(JSON.stringify({
+                          sender: 'Server',
+                          date: getTime(),
+                          command: "checktodtargetresult",
+                          message: {
+                              todid: msg.message.toId,
+                              result: (todtargetList.indexOf(msg.message.toId) >= 0) ? true : false,
+                          }
+                      }));
+
+                      break;
+
+                  case 'stayawake':
+                      //console.log('Receive stayawake from', uuid,'on', getTime());
+                      break;
+
+                  default:
+                      console.log('wss ' + this.uuid, 'send command: ', msg.command, ' is not match!');
+                      console.log("msg", msg);
+              }
+          }
+      } catch (e) {
+          console.log(e);
+      }
+
+  });
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
