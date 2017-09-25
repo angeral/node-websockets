ccdf0fdf12f30581c873c0d83842265547abca7e
angeral <32188027+angeral@users.noreply.github.com>
2017-09-25 11:42:57 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ wss.on('connection', (ws) => {
   uuidList.push(uuid);
 
   ws.on('message', function incoming(message) {
-      this.uuid = uuid;
+      //this.uuid = uuid;
       //connection = this;
       //connection.uuid = uuid;
       connectList[uuid] = {
